f97e8a1737ad97f73ec0027376ee31bb87b17d72
probee <lebetlaf@gmail.com>
2017-01-05 22:23:11 +0000

    script pour le carousel ajouter

--- amcsite/assets/js/default.js
+++ amcsite/assets/js/default.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function($) {
     new WOW().init();
+    // parallax initialization
     $('.parallax').parallax();
+    // news slider initialization
     $('.bxslider').bxSlider({
         auto: true,
         controls: false,
@@ -9,6 +11,19 @@ jQuery(document).ready(function($) {
         autoHover: true
 
     });
+    // script pour le carousel références
+    $('.slider').bxSlider({
+        slideWidth: 200,
+        minSlides: 3,
+        maxSlides: 6,
+        slideMargin: 15,
+        auto: true,
+        controls: true,
+        pager: false,
+        pause: 5000,
+        autoHover: true
+    });
+    // mobile navigation setup
     $('.button-collapse').sideNav({
             menuWidth: 250, // Default is 240
             edge: 'left', // Choose the horizontal origin
