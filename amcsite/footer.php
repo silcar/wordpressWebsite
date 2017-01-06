3b3d02e1e7023223de2dc1b621af902cfd29e0f4
probee <lebetlaf@gmail.com>
2017-01-06 00:28:17 +0000

    Responsive footer et liens automatisés dans le backoffice

--- amcsite/footer.php
+++ amcsite/footer.php
@@ -1,32 +1,37 @@
 
 </main><!-- /main -->
-<footer class="footer" style="background-color: #14669B;">
-      <div class="row" style="color:#f2f2f2">
-            <div class="col s12 offset-m1 m4 center-align">
-                  <h3 style="font-size: 1.4em; text-transform: uppercase">Qui sommes nous ?</h3>
-                  <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                        Asperiores consequatur dignissimos dolore doloribus ducimus est excepturi ipsa, laudantium, modi molestiae necessitatibus nesciunt
-                        pariatur quae qui quod tempore tenetur vitae voluptate!</p>
-            </div>
-            <div class="col s12 m3 center-left">
-                  <ul>
-                        <h3 style="font-size: 1.3em; text-transform: uppercase">Menu</h3>
-                        <li><a href="">Accueil</a></li>
-                        <li><a href="">Qui sommes-nous ?</a></li>
-                        <li><a href="">Types d'intervention</a></li>
-                        <li><a href="">Domaines d'espertise</a></li>
-                        <li><a href="">Nos clients</a></li>
-                  </ul>
-            </div>
-            <div class="col s12 m3 center-left">
-                  <ul>
-                        <h3 style="font-size: 1.2em; text-transform: uppercase">Contact</h3>
-                        <li><i class="fa fa-home" style="color: #F17A21;"></i> Adresse</li>
-                        <li><i class="fa fa-phone" style="color: #F17A21;"></i> Téléphone</li>
-                        <li><i class="fa fa-envelope" style="color: #F17A21;"></i> email</li>
-                  </ul>
+<footer class="footer">
+      <div class="container">
+            <div class="row grey-text text-lighten-4">
+                  <div class="col s12 l4 center-align about">
+                        <h5>À propos de nous</h5>
+                        <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                              Asperiores consequatur dignissimos dolore doloribus ducimus est excepturi ipsa, laudantium, modi molestiae necessitatibus nesciunt
+                              pariatur quae qui quod tempore tenetur vitae voluptate!</p>
+                  </div>
+                  <div class="col s12 l4 center-left">
+                        <ul class="footer-menu">
+                              <h5>Menu</h5>
+                              <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Accueil</a></li>
+                              <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Qui sommes nous ?</a></li>
+                              <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Types d'intervention</a></li>
+                              <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Domaines d'espertise</a></li>
+                              <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Nos clients</a></li>
+                              <li><i class="fa fa-angle-right" aria-hidden="true"></i> <a href="">Espace abonnes</a></li>
+                        </ul>
+                  </div>
+                  <div class="col s12 l4 center-left">
+                        <ul class="footer-contact">
+                              <h5>Contact</h5>
+                              <li><i class="fa fa-home"></i> <span>Sicap Amitié 3 – N° 4435, BP 15708 <br>Dakar-Fann Sénégal</span></li>
+                              <li><i class="fa fa-phone"></i> <span>+221 33 859 75 00</span></li>
+                              <li><i class="fa fa-envelope"></i> <a style="font-size: .87em;" href="mailto:administration@afriquemanagementconseil.com">administration@afriquemanagementconseil.com</a></li>
+                              <li><i class="fa fa-linkedin-square" aria-hidden="true"></i> <a href="https://www.linkedin.com/company/afrique-management-conseil" target="_blank">Afrique Management conseil</a></li>
+                        </ul>
+                  </div>
             </div>
       </div>
+
 </footer>
 
 <?php wp_footer(); ?>
