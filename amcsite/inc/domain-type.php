449ae1cca2e6df57fc15dcfb1d092d77fa4fad3e
probee <lebetlaf@gmail.com>
2017-01-07 12:52:29 +0000

    Landing pour le domaine d'expertise selectionné

--- amcsite/inc/domain-type.php
+++ amcsite/inc/domain-type.php
@@ -74,17 +74,24 @@ function add_custom_meta_boxes() {
 
 } // end add_custom_meta_boxes
 add_action('add_meta_boxes', 'add_custom_meta_boxes');
-function wp_custom_attachment($id) {
+function wp_custom_attachment($post) {
 
     wp_nonce_field(plugin_basename(__FILE__), 'wp_custom_attachment_nonce');
 
-    $cover = get_post_meta($id, 'wp_custom_attachment', true);
-    $cover = $cover['url']['name'];
+    $resources = get_post_meta( $post->ID, 'wp_custom_attachment', true );
+
     $html = '<p class="description">';
     $html .= 'Télécharger image de cover';
     $html .= '</p>';
-    $html .= '<input type="file" id="wp_custom_attachment" name="wp_custom_attachment" accept="image/*" value="" size="25" />';
-
+    $html .= '<input type="file" id="wp_custom_attachment" name="wp_custom_attachment" accept="image/*" value="'. $resources['url'] .'" size="25" />';
+    if($resources){
+        $html .= '<p>';
+        $html .= '<img id="cover-img" src="'. $resources['url'] .'" style="width:245px; height:auto;">';
+        $html .= '</p>';
+        $html .= '<p>';
+        $html .= '<a href="#" id="remove-cover-img" style="cursor:pointer; text-decoration: underline">Supprimer l\'image de couverture</a>';
+        $html .= '</p>';
+    }
     echo $html;
 
 } // end wp_custom_attachment
