b2b3d91bdcfd3851a82c32ca7ec5146381582476
probee <lebetlaf@gmail.com>
2017-01-08 21:29:58 +0000

    image de couverture qui s'affiche après importation contenu

--- amcsite/single-domain.php
+++ amcsite/single-domain.php
@@ -1,15 +1,18 @@
 <?php get_header(); ?>
 
 <div class="row main-domain">
-        <?php while (have_posts()) : the_post();
-                $thumb_id = get_post_thumbnail_id();
-        $cover = get_post_meta(get_the_ID(), 'wp_custom_attachment', true);
-                $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-                $thumb_url = $thumb_url_array[0];
-        ?>
-    <div class="row">
+    <?php while (have_posts()) : the_post();
+    $thumb_id = get_post_thumbnail_id();
+    $cover = get_post_meta(get_the_ID(), 'wp_custom_attachment', true);
+
+    $expStr = explode("/uploads/", $cover['url']);
+    $uploads = wp_upload_dir();
+    $upload_path = $uploads['baseurl'];
+    $cover_url = $upload_path . '/' . $expStr[1];
+    ?>
+    <div class="row" style="max-height: 355px; overflow-y: hidden">
         <?php if($cover): ?>
-        <img class="responsive-img" src="<?= $cover['url']; ?>" alt="illustration-<?php the_title(); ?>">
+            <img class="responsive-img" src="<?= $cover_url; ?>" alt="illustration-<?php the_title(); ?>">
         <?php endif; ?>
     </div>
     <div class="container">
@@ -17,7 +20,7 @@
         <div class="domain-content">
             <?php the_content(); ?>
         </div>
-            <?php endwhile; ?>
+        <?php endwhile; ?>
     </div>
     <div class="row related-type">
 
