d7ffe616eff4f77fd108e325403d19d8a20f23f5
probee <lebetlaf@gmail.com>
2017-01-09 11:20:52 +0000

    Fixer le scroll pour les actualités avec long texte

--- amcsite/front-page.php
+++ amcsite/front-page.php
@@ -50,140 +50,140 @@
             </div>
         </nav>
     </header>
-<!-- End Header
-<!-- Content -->
-<?php
-$args = array(
-    'post_type' => 'actualite',
-    'posts_per_page' => 100,
-    'orderby' => array(
-        'id' => 'DESC'
-    )
-);
-$actualite = new WP_Query( $args );
-?>
-   <div id="home"  class=" section scrollspy main-content">
-       <img class="responsive-img" src="<?= get_template_directory_uri().'/assets/img/header_img.jpg' ;?>" alt="Header Image">
-   </div>
-<div class="col s12 news">
-    <h1 class="center-align news-title"><a href="#">Actualites</a></h1>
-    <div class="bxslider">
-        <?php if($actualite->have_posts() === true): ?>
-            <?php
-            while ( $actualite->have_posts() ) : $actualite->the_post();
-                $desc = get_the_content();
-                ?>
-                <div class="news-description">
-                    <?= $desc;?>
-                    <a href="" class="news-more">Lire la suite</a></div>
+    <!-- End Header
+    <!-- Content -->
+    <?php
+    $args = array(
+        'post_type' => 'actualite',
+        'posts_per_page' => 100,
+        'orderby' => array(
+            'id' => 'DESC'
+        )
+    );
+    $actualite = new WP_Query( $args );
+    ?>
+    <div id="home"  class=" section scrollspy main-content">
+        <img class="responsive-img" src="<?= get_template_directory_uri().'/assets/img/header_img.jpg' ;?>" alt="Header Image">
+    </div>
+    <div class="col s12 news">
+        <h1 class="center-align news-title"><a href="#">Actualites</a></h1>
+        <div class="bxslider">
+            <?php if($actualite->have_posts() === true): ?>
                 <?php
-            endwhile;
-            wp_reset_postdata(); ?>
-        <?php else: ?>
-            <div class="news-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Ad iure odit officia? A aliquam autem cupiditate dolorem, doloribus ea eligendi incidunt ipsam itaque iure mollitia neque optio praesentium ...
-                <a href="" class="news-more">Lire la suite</a></div>
-            <div class="news-description">Dolor sit amet, consectetur adipisicing elit. Corporis culpa doloribus, ea impedit iusto nisi quo. Accusantium architecto dicta dolor ipsam iste itaque, nisi optio quis rem veritatis. Repellendus, voluptatem.
-                upiditate igendi incidunt ipsam itaque iure mollitia neque optio praesentium ...
-                <a href="" class="news-more">Lire la suite</a></div>
-        <?php endif; ?>
-    </div>
-
-</div>
-<div id="type" class="col s12 section scrollspy news" style="color: #FFF; background-color: #225D89; padding:10px">
-    <h1 class="center-align news-title"><a href="#" style="color: #FFF;">Types d'intervention</a></h1>
-    <div class="col s12 in__content"><?php
-        $args = array(
-            'post_type' => 'expertise',
-            'posts_per_page' => 100,
-            'orderby' => array(
-                'id' => 'DESC'
-            )
-        );
-        $t_expertise = new WP_Query( $args );
-
-$get_experts = wp_count_posts('expertise');
-$count_expert = $get_experts->publish;
-        $j = intval($count_expert);
-        $i = 1;?><?php
-        while ( $t_expertise->have_posts() ) : $t_expertise->the_post();
-            if($i == 1 ){
-                echo "<div class=\"col s6 right-align wow slideInLeft\"><ul class=\"col-left\">";
-            }
-            ?>
-            <li><a class="intervention-link" href="<?php the_permalink() ;?>"><?= the_title(); ?></a></li>
-        <?php
-            if ($i == $j/2){
-                echo "</ul>
-                </div>
-                <div class=\"col s6 left-align wow slideInRight\">
-                    <ul class=\"col-right\">";
-            }
-            if ($i == $j){
-                echo "</ul>
-                </div>";
-            }
-            $i++;
-            ?>
-            <?php
-        endwhile;
-        wp_reset_postdata(); ?>
+                while ( $actualite->have_posts() ) : $actualite->the_post();
+                    $desc = get_the_content();
+                    ?>
+                    <div class="news-description">
+                        <?= shorten_string($desc, 30);?>
+                        <a href="" class="news-more">Lire la suite</a></div>
+                    <?php
+                endwhile;
+                wp_reset_postdata(); ?>
+            <?php else: ?>
+                <div class="news-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Ad iure odit officia? A aliquam autem cupiditate dolorem, doloribus ea eligendi incidunt ipsam itaque iure mollitia neque optio praesentium ...
+                    <a href="" class="news-more">Lire la suite</a></div>
+                <div class="news-description">Dolor sit amet, consectetur adipisicing elit. Corporis culpa doloribus, ea impedit iusto nisi quo. Accusantium architecto dicta dolor ipsam iste itaque, nisi optio quis rem veritatis. Repellendus, voluptatem.
+                    upiditate igendi incidunt ipsam itaque iure mollitia neque optio praesentium ...
+                    <a href="" class="news-more">Lire la suite</a></div>
+            <?php endif; ?>
+        </div>
 
     </div>
-    <div class="clearfix"></div>
-</div>
-    <div class="parallax-container">
-        <div class="parallax"><img src="<?= get_template_directory_uri().'/assets/img/img_content.jpg' ;?>" style="opacity: .8;"></div>
-    </div>
-<div id="domain" class="section scrollspy wow bounceInUp" style="background-color: #F17A21;">
-    <div class="row" style="margin-bottom:0px">
-        <h1 class="center-align dn-title"><a href="#">Domaines d'expertise</a></h1>
-        <div class="row box-wrapper">
-            <?php
+    <div id="type" class="col s12 section scrollspy news" style="color: #FFF; background-color: #225D89; padding:10px">
+        <h1 class="center-align news-title"><a href="#" style="color: #FFF;">Types d'intervention</a></h1>
+        <div class="col s12 in__content"><?php
             $args = array(
-                'post_type' => 'domain',
+                'post_type' => 'expertise',
                 'posts_per_page' => 100,
                 'orderby' => array(
                     'id' => 'DESC'
                 )
             );
-            $domain = new WP_Query( $args );
-            $i = 1;?>
-            <?php
-            while ( $domain->have_posts() ) : $domain->the_post();
-                $thumb_id = get_post_thumbnail_id();
-                $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
-                $thumb_url = $thumb_url_array[0];
+            $t_expertise = new WP_Query( $args );
+
+            $get_experts = wp_count_posts('expertise');
+            $count_expert = $get_experts->publish;
+            $j = intval($count_expert);
+            $i = 1;?><?php
+            while ( $t_expertise->have_posts() ) : $t_expertise->the_post();
+                if($i == 1 ){
+                    echo "<div class=\"col s6 right-align wow slideInLeft\"><ul class=\"col-left\">";
+                }
                 ?>
-                <div class="col s12 m6 l4 box">
-                    <div class="white wow fadeIn" data-wow-delay="1s" style="padding: 10px 15px; position:relative">
-                        <a href="<?php the_permalink();?>" style="position:absolute; display: block; width: 100%; height: 100%;"></a>
-                       <div class="card-header valign-wrapper center-align"><img src="<?= $thumb_url ?>" alt=""> <?php the_title( '<h3>', '</h3>' ) ?></div>
-                        <div class="card-content"><?php the_excerpt();?></div>
-                    </div>
+                <li><a class="intervention-link" href="<?php the_permalink() ;?>"><?= the_title(); ?></a></li>
+                <?php
+                if ($i == $j/2){
+                    echo "</ul>
                 </div>
+                <div class=\"col s6 left-align wow slideInRight\">
+                    <ul class=\"col-right\">";
+                }
+                if ($i == $j){
+                    echo "</ul>
+                </div>";
+                }
+                $i++;
+                ?>
                 <?php
             endwhile;
             wp_reset_postdata(); ?>
-            </div>
-    </div>
-</div>
-<div class="row ">
-    <img class="responsive-img" src="<?= get_template_directory_uri().'/assets/img/img_bottom.jpg' ;?>" alt="Bottom image">
-</div>
-<div  class="row white">
-    <h1 id="reference" class=" section scrollspy center-align dn-title"><a href="#" style="color: #F17A21;">References</a></h1>
-    <div class="slider valign-wrapper">
-        <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/eurogerm_logo.png" alt=""></div>
-        <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/gainde2000_logo.png" alt=""></div>
-        <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/senegalaiseauto_logo.png" alt=""></div>
-        <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/lcs_logo.png" alt=""></div>
-        <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/matforce_logo.png" alt=""></div>
-        <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/total_logo.jpg" alt=""></div>
-        <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/sedima_logo.png" alt=""></div>
-    </div>
 
-</div>
-<?php get_footer(); ?>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <div class="parallax-container">
+        <div class="parallax"><img src="<?= get_template_directory_uri().'/assets/img/img_content.jpg' ;?>" style="opacity: .8;"></div>
+    </div>
+    <div id="domain" class="section scrollspy wow bounceInUp" style="background-color: #F17A21;">
+        <div class="row" style="margin-bottom:0; padding-top:20px;">
+            <h1 class="center-align dn-title"><a href="#">Domaines d'expertise</a></h1>
+            <div class="row box-wrapper">
+                <?php
+                $args = array(
+                    'post_type' => 'domain',
+                    'posts_per_page' => 100,
+                    'orderby' => array(
+                        'id' => 'DESC'
+                    )
+                );
+                $domain = new WP_Query( $args );
+                $i = 1;?>
+                <?php
+                while ( $domain->have_posts() ) : $domain->the_post();
+                    $thumb_id = get_post_thumbnail_id();
+                    $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+                    $thumb_url = $thumb_url_array[0];
+                    ?>
+                    <div class="col s12 m6 l4 box">
+                        <div class="white wow fadeIn" data-wow-delay="1s" style="padding: 10px 15px; position:relative">
+                            <a href="<?php the_permalink();?>" style="position:absolute; display: block; width: 100%; height: 100%;"></a>
+                            <div class="card-header valign-wrapper center-align"><img src="<?= $thumb_url ?>" alt=""> <?php the_title( '<h3>', '</h3>' ) ?></div>
+                            <div class="card-content"><?php the_excerpt();?></div>
+                        </div>
+                    </div>
+                    <?php
+                endwhile;
+                wp_reset_postdata(); ?>
+            </div>
+        </div>
+    </div>
+    <div class="row ">
+        <img class="responsive-img" src="<?= get_template_directory_uri().'/assets/img/img_bottom.jpg' ;?>" alt="Bottom image">
+    </div>
+    <div id="reference"  class="section scrollspy row white" style="padding-top:20px;">
+        <h1 class=" center-align dn-title"><a href="#" style="color: #F17A21;">References</a></h1>
+        <div class="slider valign-wrapper">
+            <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/eurogerm_logo.png" alt=""></div>
+            <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/gainde2000_logo.png" alt=""></div>
+            <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/senegalaiseauto_logo.png" alt=""></div>
+            <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/lcs_logo.png" alt=""></div>
+            <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/matforce_logo.png" alt=""></div>
+            <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/total_logo.jpg" alt=""></div>
+            <div class="slide"><img src="<?= get_template_directory_uri() ?>/assets/img/logos/sedima_logo.png" alt=""></div>
+        </div>
+
+    </div>
+    <?php get_footer(); ?>
 </body>
 </html>
